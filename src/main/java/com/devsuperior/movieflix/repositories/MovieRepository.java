b7d3f65ae1891cdc4d0a90b95a511d49c7696792
Gabriel Garcia <gabrielproaj@gmail.com>
2026-01-04 09:33:46 -0300

    Repositories

---
+++ src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
@@ -0,0 +1,20 @@
+package com.devsuperior.movieflix.repositories;
+
+import com.devsuperior.movieflix.entities.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    @Query("""
+            SELECT m
+            FROM Movie m
+            WHERE (:genreId IS NULL OR m.genre.id = :genreId)
+            ORDER BY m.title.name
+            """)
+    Page<Movie> moviesByGenre(Long genreId, Pageable pageable);
+}
